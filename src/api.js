1b0f53d99cd8e8518139491b94914173e96e8c1f
RajanSingh-132 <singhrajan7052@gmail.com>
2026-03-06 18:27:17 +0530

    mod

--- src/api.js
+++ src/api.js
@@ -1,29 +1,29 @@
 import axios from "axios";
 
-const BASE_URL =
-  process.env.NEXT_PUBLIC_BACKEND_URL ||
-  "http://127.0.0.1:8000";
-
+const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+// console.log("BACKEND:", process.env.REACT_APP_BACKEND_URL);
 export const sendMessage = async (data) => {
   try {
+
     const res = await axios.post(
       `${BASE_URL}/chat`,
       data,
       {
         headers: {
-          "Content-Type": "application/json",
-        },
-        timeout: 30000,
+          "Content-Type": "application/json"
+        }
       }
     );
 
     return res.data;
 
   } catch (error) {
+
     console.error("API ERROR:", error?.response?.data || error.message);
 
     return {
-      response: "⚠️ Unable to reach AI server.",
+      response: "⚠️ Unable to reach AI server."
     };
+
   }
 };
